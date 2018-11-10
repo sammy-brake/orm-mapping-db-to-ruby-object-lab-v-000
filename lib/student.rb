d4625c129b4696c81aed1975c67c7a6d9a9e7a26
Samantha brake <sammy.brake@gmail.com>
2018-11-10 00:21:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,8 +99,8 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql).flatten.map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row).flatten
     end
   end
 
