9a8c2a9c333bc1811d4835183271014a09e1ae7f
Samantha brake <sammy.brake@gmail.com>
2018-11-10 00:22:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,9 +99,9 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row).flatten
-    end
+    
+      self.new_from_db(DB[:conn].execute(sql))
+    
   end
 
   def all_students_in_grade_X(grade)
